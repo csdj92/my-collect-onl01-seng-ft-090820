7a67064fc6cd88c522501691302d4b5aa5b04bd7
CJ Davis <csdj92@gmail.com>
2020-09-10 18:30:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,23 +11,3 @@ def my_collect(array)
   end
 
 
-def my_collect(students) 
- i=0
-  students.split(" ").first = []
-   while i < students.length
-   students << yield (students[i])
-    i += 1
-  end
-   students
-  end 
-
-
-def my_collect(language) 
-   i=0
-  language = []
-   while i < language.length
-   language << yield (language[i])
-    i += 1
-  end
-   language
-  end 
